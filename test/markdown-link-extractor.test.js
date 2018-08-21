97c251b5bfd86b0ede38e488860f5162e7dabed9
angiecortez <angiecorteztay1@hotmail.com>
2018-08-21 01:08:51 -0500

    [ADD] thank you, i will nott kill myself

--- test/markdown-link-extractor.test.js
+++ test/markdown-link-extractor.test.js
@@ -15,50 +15,50 @@ describe('markdown-link-extractor', function () {
         var links = markdownLinkExtractor('[example](http://www.example.com)');
         expect(links).to.be.an('array');
         expect(links).to.have.length(1);
-        expect(links[0]).to.be('http://www.example.com');
+        expect(links[0].href.href).to.be('http://www.example.com');
     });
 
     it('should extract a link in a with escaped braces [tag](http://example.com\(1\))', function () {
         var links = markdownLinkExtractor('[XMLHttpRequest](http://msdn.microsoft.com/library/ie/ms535874\\(v=vs.85\\).aspx)');
         expect(links).to.be.an('array');
         expect(links).to.have.length(1);
-        expect(links[0]).to.be('http://msdn.microsoft.com/library/ie/ms535874(v=vs.85).aspx');
+        expect(links[0].href).to.be('http://msdn.microsoft.com/library/ie/ms535874(v=vs.85).aspx');
     });
 
     it('should extract an image link in a ![tag](http://example.com/image.jpg)', function () {
         var links = markdownLinkExtractor('![example](http://www.example.com/image.jpg)');
         expect(links).to.be.an('array');
         expect(links).to.have.length(1);
-        expect(links[0]).to.be('http://www.example.com/image.jpg');
+        expect(links[0].href).to.be('http://www.example.com/image.jpg');
     });
 
     it('should extract an image link in a ![tag](foo/image.jpg)', function () {
         var links = markdownLinkExtractor('![example](foo/image.jpg)');
         expect(links).to.be.an('array');
         expect(links).to.have.length(1);
-        expect(links[0]).to.be('foo/image.jpg');
+        expect(links[0].href).to.be('foo/image.jpg');
     });
 
     it('should extract an image link in a ![tag](foo/image.jpg =20%x50)', function () {
         var links = markdownLinkExtractor('![example](foo/image.jpg =20%x50)');
         expect(links).to.be.an('array');
         expect(links).to.have.length(1);
-        expect(links[0]).to.be('foo/image.jpg');
+        expect(links[0].href).to.be('foo/image.jpg');
     });
 
     it('should extract a bare link http://example.com', function () {
         var links = markdownLinkExtractor('This is a link: http://www.example.com');
         expect(links).to.be.an('array');
         expect(links).to.have.length(1);
-        expect(links[0]).to.be('http://www.example.com');
+        expect(links[0].href).to.be('http://www.example.com');
     });
 
     it('should extract multiple links', function () {
         var links = markdownLinkExtractor('This is an [example](http://www.example.com). Hope it [works](http://www.example.com/works)');
         expect(links).to.be.an('array');
         expect(links).to.have.length(2);
-        expect(links[0]).to.be('http://www.example.com');
-        expect(links[1]).to.be('http://www.example.com/works');
+        expect(links[0].href).to.be('http://www.example.com');
+        expect(links[1].href).to.be('http://www.example.com/works');
     });
 
 });
